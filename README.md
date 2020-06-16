e011dbf30771100034163ad8a710c3cde2a6f3a3
Hramos <ramoshumbertoa6@gmail.com>
2020-06-16 13:08:09 -0400

    Add Source

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 # Covid19_Ve
 This is a Repository where I going to upload processing data of Covid in Venezuela 
 
-# Firts Commit
+# Source
+```python
+URL = 'https://covid19.patria.org.ve/api/v1/timeline'
+```
---
+++ app/getdata.py
@@ -0,0 +1,31 @@
+import requests
+
+def getData(URL):
+    r = requests.get(URL)
+    data = r.json()
+    Date = []
+    DateTS = []
+    Confirmed_Count = []
+    Confirmed_New = []
+    Recovered_Count = []
+    Recovered_New = []
+    Deaths_Count = []
+    Deaths_New = []
+    Active_Count = []
+    for i in range(len(data)):
+        DateTS.append(data[i]['DateTS'])
+    for i in range(len(data)):
+        Date.append(data[i]['Date'])
+    for i in range(len(data)):
+        Confirmed_Count.append(data[i]['Confirmed']['Count'])
+        Confirmed_New.append(data[i]['Confirmed']['New'])
+    for i in range(len(data)):
+        Recovered_Count.append(data[i]['Recovered']['Count'])
+        Recovered_New.append(data[i]['Recovered']['New'])
+    for i in range(len(data)):
+        Deaths_Count.append(data[i]['Deaths']['Count'])
+        Deaths_New.append(data[i]['Deaths']['New'])
+    for i in range(len(data)):
+        Active_Count.append(data[i]['Active']['Count'])
+    x = list(zip(Date,DateTS,Confirmed_Count,Confirmed_New,Recovered_Count,Recovered_New,Deaths_Count,Deaths_New,Active_Count))
+    return x
