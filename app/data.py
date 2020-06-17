8b5c4b3183fe9aac03c75a64ccb55c5572a719e6
Hramos <ramoshumbertoa6@gmail.com>
2020-06-17 14:59:09 -0400

    Add Graph

--- app/data.py
+++ app/data.py
@@ -1,5 +1,7 @@
 from app.getdata import getData
 import pandas as pd 
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 URL = 'https://covid19.patria.org.ve/api/v1/timeline'
 
@@ -7,3 +9,25 @@ df = pd.DataFrame(getData(URL), columns=['Date','DateTS','Confirmed_Count','Conf
     'Recovered_New','Deaths_Count','Deaths_New','Active_Count'])
 
 head = df.tail()
+
+
+plt.figure(figsize=(10,7.5)) 
+with plt.style.context('Solarize_Light2'):
+    plt.plot(df['Date'], df['Confirmed_Count'])
+    plt.legend('Confirmed')
+    plt.plot(df['Date'], df['Deaths_Count'])
+    plt.legend('Deaths')
+    plt.plot(df['Date'], df['Recovered_New'])
+    plt.legend('Recovered')
+    plt.plot(df['Date'], df['Active_Count'])
+    plt.legend('Active')
+    
+
+    # Number of accent colors in the color scheme
+    
+    plt.title('8 Random Lines - Line')
+    plt.xlabel('x label', fontsize=14)
+    plt.ylabel('y label', fontsize=14)
+
+plt.savefig('app/static/graph.png')
+
--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,5 @@
 from app import app
-from flask import render_template
+from flask import render_template, url_for
 
 
 from app.getdata import getData
@@ -9,4 +9,4 @@ from app.data import *
 @app.route('/index', methods=('POST', 'GET'))
 def index():
     user = { ' username': 'Humberto Ramos'}
-    return render_template('index.html', tables = [head.to_html(classes='data')], title='Home', user=user)
+    return render_template('index.html', tables = [head.to_html(classes='table table-striped')], title='Home', user=user)
