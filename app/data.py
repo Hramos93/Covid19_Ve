24e3efde1c78774bfd993135c5e39ab58c78dfd5
Hramos <ramoshumbertoa6@gmail.com>
2020-06-21 14:12:20 -0400

    Add Graph

--- app/data.py
+++ app/data.py
@@ -3,38 +3,27 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 from datetime import datetime
+import matplotlib.dates as mdates
 
 
 URL = 'https://covid19.patria.org.ve/api/v1/timeline'
 
 df = pd.DataFrame(getData(URL), columns=['Date','DateTS','Confirmed_Count','Confirmed_New','Recovered_Count',\
     'Recovered_New','Deaths_Count','Deaths_New','Active_Count'])
+df['Date']  = pd.to_datetime(df['Date'])
 df = df.drop(['DateTS','Recovered_New','Deaths_New','Active_Count'], axis=1)
 
 df_tail = df.tail()
-now = datetime.utcnow().strftime('%B %d %Y' )
+now = datetime.today().strftime('%B %d %Y' )
 
 
-"""
+plt.style.use('dark_background')
 
-plt.figure(figsize=(10,7.5)) 
-with plt.style.context('Solarize_Light2'):
-    plt.plot(df['Date'], df['Confirmed_Count'])
-    plt.legend('Confirmed')
-    plt.plot(df['Date'], df['Deaths_Count'])
-    plt.legend('Deaths')
-    plt.plot(df['Date'], df['Recovered_New'])
-    plt.legend('Recovered')
-    plt.plot(df['Date'], df['Active_Count'])
-    plt.legend('Active')
-    
-
-    # Number of accent colors in the color scheme
-    
-    plt.title('8 Random Lines - Line')
-    plt.xlabel('x label', fontsize=14)
-    plt.ylabel('y label', fontsize=14)
+plt.plot(df['Date'], df['Confirmed_Count'] ,label = "Confirmed_Count")
+plt.plot(df['Date'], df['Recovered_Count'], color='green',label = "Recovered_Count")
+plt.plot(df['Date'], df['Deaths_Count'],  color='red',label = "Deaths_Count")
+plt.tick_params(direction='out', length=6, width=2, colors='orange',\
+            grid_color='r', grid_alpha=0.5,labelrotation= 30.0)
+plt.legend()
 
 plt.savefig('app/static/graph.png')
-
-"""
